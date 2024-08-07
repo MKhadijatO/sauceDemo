ff944a8dd3b1b7d474cf69525a5aa06b95391040
MKhadijatO <muhammadkhadijato@gmail.com>
2024-08-07 21:34:06 +0100

    Edit

--- cypress/integration/BDD/Menu/menu.js
+++ cypress/integration/BDD/Menu/menu.js
@@ -8,11 +8,7 @@ beforeEach(function () {
     this.data = data;
   });
 
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-  });
+  
 });
 
 //@closebutton
