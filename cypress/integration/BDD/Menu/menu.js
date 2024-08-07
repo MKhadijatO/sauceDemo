2da6d12dfd06eecba7118709929d9c395e0e73fe
MKhadijatO <muhammadkhadijato@gmail.com>
2024-08-07 21:08:27 +0100

    Update

--- cypress/integration/BDD/Menu/menu.js
+++ cypress/integration/BDD/Menu/menu.js
@@ -7,12 +7,6 @@ beforeEach(function () {
   cy.fixture("example").then(function (data) {
     this.data = data;
   });
-
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-  });
 });
 
 //@closebutton
