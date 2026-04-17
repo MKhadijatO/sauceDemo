ae7c95f082244ad9d2730664e4b0f7325c6ef2e0
Khadijat <muhammadkhadijato@gmail.com>
2026-04-17 22:45:25 +0100

    fixed conflicts

--- cypress/integration/BDD/checkoutProcess/checkoutProcess.js
+++ cypress/integration/BDD/checkoutProcess/checkoutProcess.js
@@ -11,7 +11,7 @@ beforeEach(function () {
 
 //@checkoutProcess
 Given("I visit Saucedemo E-commerce website", function() {
-  cy.visit("https://www.saucedemo.com/");
+  cy.visit("https://www.saucedemo.com");
 });
 
 When("I login into the website", function () {
